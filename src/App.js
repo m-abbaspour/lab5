6bef603fc038d6fdfb01d14255545fc7c2fe0887
Maddie Abbaspour <89539144+m-abbaspour@users.noreply.github.com>
2022-07-11 00:10:38 -0600

    Update App.js
    Removes debugging files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Form from './Components/PartB/Form';
 import { TableHttp } from './Components/PartA/TableHttp';
-import Table from './Components/PartA/Table';
 
 function App() {
   return (
     <div className="App">
       <TableHttp/>
-      {/* <Table/> */}
       {/* <Form/> */}
     </div>
   );
